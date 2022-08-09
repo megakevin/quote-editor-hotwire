8b75ca6592f61e94d22fae032a428c60fad5c1cc
Kevin Campusano <kcampusano@endpointdev.com>
2022-08-09 00:08:31 -0400

    Add current company helper method to application controller.
    Make it also available as a view helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # before_action -> { sleep 1 }
   before_action :authenticate_user!, unless: :devise_controller?
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
 end
