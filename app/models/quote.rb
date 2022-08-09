e14a5b1375dfc106e9ec1bfa4d6ff23b136672a8
Kevin Campusano <kcampusano@endpointdev.com>
2022-08-08 23:00:38 -0400

    Implement real time update of quotes list with turbo streams and active cable.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,13 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
