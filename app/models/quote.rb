36696232288c77310153f9d7d18c64fb521c09a9
Kevin Campusano <kcampusano@endpointdev.com>
2022-08-10 23:29:30 -0400

    Compartmentalize quotes stream by company.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -12,5 +12,5 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
