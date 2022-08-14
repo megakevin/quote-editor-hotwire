b1e89dc9c73f89949fda1912b2362f16b710f4b3
Kevin Campusano <kcampusano@endpointdev.com>
2022-08-13 22:04:56 -0400

    Render nice message when no quotes exists.
    Via CSS. Also, add alternative implementation using turbo as comments.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,8 +9,25 @@ class Quote < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
 
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to [company, "quotes"] }
+  # after_destroy_commit -> { broadcast_remove_to [company, "quotes"] }
+
+  # after_create_commit do
+  #   broadcast_prepend_to(
+  #     [company, "quotes"],
+  #     partial: "quotes/quote",
+  #     locals: { quote: self },
+  #     target: "quotes"
+  #   )
+
+  #   if company.quotes.count == 1
+  #     broadcast_update_to(
+  #       [company, "quotes"],
+  #       target: Quote.new,
+  #       html: ""
+  #     )
+  #   end
+  # end
 
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
